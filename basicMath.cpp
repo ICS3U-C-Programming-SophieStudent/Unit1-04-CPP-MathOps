4007e9aa846edc28b43e80a0a6fff73ee2833010
Sophie Student <sophie.student16@gmail.com>
2026-09-12 22:46:52 +0000

    First commit of basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2026 Sophie Student All rights reserved.
+// .
+// Created by: Sophie Student
+// Date: Sep. 12, 2026
+// This program displays various math equations.
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "2+3 = " << 2+3 << std::endl;  // addition
+    std::cout << "8-4 = " << 8-4 << std::endl;  // subtraction
+    std::cout << "5*3 = " << 5*3 << std::endl;  // multiplication
+    std::cout << "10/5 = " << 10/5 << std::endl;  // integer division
+    std::cout << "7/2 = " << 7.0/2 << std::endl;  // decimal division
+    std::cout << "11 % 4 = " << 11 % 4 << std::endl;  // modulo
+    std::cout << "5^2 = " << pow(5, 2) << std::endl;  // exponents
+    std::cout << "16^0.5 = " << pow(16, 0.5) << std::endl;  // exponents
+    std::cout << "√16 = " << sqrt(16) << std::endl;  // square root
+}
